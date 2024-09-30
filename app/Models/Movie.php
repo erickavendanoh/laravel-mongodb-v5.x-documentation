469aa287f3eff5d04ef26ef40953063292f844c3
erickavendanoh <erick-armando167@outlook.es>
2024-09-30 12:21:42 -0600

    Termino de sección " Quick Start\

---
+++ app/Models/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+    protected $fillable = ['title', 'year', 'runtime', 'imdb', 'plot'];
+}
